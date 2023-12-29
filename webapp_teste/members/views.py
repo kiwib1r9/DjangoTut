14b59729f07dfa2ded76562ba3f0fe091623293d
kiwib1r9 <kiwib1r9@gmail.com>
2023-12-29 15:19:10 -0300

    all members update

--- webapp_teste/members/views.py
+++ webapp_teste/members/views.py
@@ -1,8 +1,13 @@
 from django.template import loader
 from django.http import HttpResponse
+from .models import Member
 
 def members(request):
-    template = loader.get_template('teste.html')
-    return HttpResponse(template.render())
+    mymembers = Member.objects.all().values()
+    template = loader.get_template('all_members.html')
+    context = { 
+        'mymembers' : mymembers,
+    }
+    return HttpResponse(template.render(context, request))
 
-# Create your views here.
+
